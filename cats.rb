32d544fd3b2f02f56a8de4856b25d89f3b36aac9
funzone36 <funzone36@gmail.com>
2016-06-02 13:18:03 -0400

    Created two instances of cat

--- cats.rb
+++ cats.rb
@@ -1,8 +1,11 @@
 class Cat
 
-  attr_accessor :name, :preferred_food, meal_time 
-  def method_name
+  attr_accessor :name, :preferred_food, :meal_time
 
+  def initialize(name, preferred_food, meal_time)
+        @name = name
+        @preferred_food = preferred_food
+        @meal_time = meal_time
   end
 
 
