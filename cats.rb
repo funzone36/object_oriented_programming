5b1f8a847cd8c61d9a20ca650967725f3faeb7a4
funzone36 <funzone36@gmail.com>
2016-06-02 14:30:54 -0400

    Finished exercise 1

--- cats.rb
+++ cats.rb
@@ -8,5 +8,25 @@ class Cat
         @meal_time = meal_time
   end
 
+  def eats_at(meal_time)
+      day = ""
+      @meal_time = meal_time
+      if @meal_time > 11
+        day = "PM"
+        @meal_time -= 12
+      else
+        day = "AM"
+      end
+      @meal_time.to_s + " " + day
+  end
+
+  def meow
+   "My name is #{@name} and I eat #{@preferred_food} at " + eats_at(@meal_time)
+  end
 
 end
+
+jason = Cat.new('Jason', 'orange', 20)
+fred = Cat.new('Fred', 'apple', 8)
+puts jason.meow
+puts fred.meow
